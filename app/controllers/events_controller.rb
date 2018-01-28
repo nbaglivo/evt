3180c65f1a24ac5fb04f52d2f2fb87ac3e515527
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2018-01-28 19:56:49 +0100

    controllers events/:id: Implement GET method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show]
 
   def index
     @events = Event.where(owner_id: current_user.id)
@@ -10,9 +11,21 @@ class EventsController < ApplicationController
     json_response(@event, :created)
   end
 
+  def show
+    json_response(@event)
+  end
+
   private
 
   def event_params
     params.permit(:name, :date)
+  end
+
+  def user_events
+    Event.where(owner_id: current_user.id)
+  end
+
+  def set_event
+    @event = user_events.find(params[:id])
   end
 end
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Events API', type: :request do
   let(:user) { create(:user) }
   let!(:user_events) { create_list(:event, 10, owner: user) }
+  let(:event_id) { user_events.first.id }
 
   let!(:other_user_events) { create_list(:event, 10) }
+
 
   # authorize request
   let(:headers) { valid_headers }
@@ -68,6 +70,48 @@ RSpec.describe 'Events API', type: :request do
       it 'returns a validation failure message' do
         expect(response.body)
           .to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  describe 'GET /events/:id' do
+    before { get "/events/#{event_id}", params: {}, headers: headers }
+
+    context 'when the record exists' do
+
+      context 'when the record belongs to the authenticated user' do
+        it 'returns the event' do
+          expect(json).not_to be_empty
+          expect(json['id']).to eq(event_id)
+        end
+
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'when the record does not belong to the authenticated user' do
+        let(:event_id) { other_user_events.first.id }
+
+        it 'returns status code 404' do
+          expect(response).to have_http_status(404)
+        end
+
+        it 'returns a not found message' do
+          expect(response.body).to match(/Couldn't find Event/)
+        end
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:event_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Event/)
       end
     end
   end
